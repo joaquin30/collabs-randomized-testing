2421557d5b6ab0ab7faa05fa278eac5e45343341
j30 <46060104+joaquin30@users.noreply.github.com>
2024-12-11 19:50:41 -0500

    añadido resultados originales y README

---
+++ README.md
@@ -0,0 +1,11 @@
+# Implementacion de "Verificación de consistencia eventual en programas que utilicen CRDTs mediante pruebas aleatorizadas"
+
+Para correr el experimento se requiere NodeJS y ejecutar los siguientes comandos:
+
+```
+npm install
+npx tsc
+node dist/experiment.js
+```
+
+El experimento no es determinístico así que los valores varían un poco. Los resultados originales se encuentran en el archivo `results.json`. El formato de los experimentos ha cambiado y la explicación del nuevo formato se encuentra en `src/experiment.ts`.
--- src/experiment.ts
+++ src/experiment.ts
@@ -5,21 +5,22 @@ const deliveryLikehood: number[] = [30, 40, 50, 60, 70];
 const iterations = 1000;
 const tests: object[] = [];
 
-for (let i of deliveryOrderBias) {
-  for (let j of deliveryLikehood) {
+for (let i of deliveryLikehood) {
+  for (let j of deliveryOrderBias) {
     let numTrue = 0
     const durations: number[] = [];
     for (let k = 0; k < iterations; k++) {
       const start = Date.now();
-      const result = runTest({deliveryOrderBias: i, deliveryLikehood: j});
+      const result = runTest({deliveryLikehood: i, deliveryOrderBias: j});
       durations.push(Date.now() - start);
       if (result) numTrue++;
     }
     tests.push({
-      "iterations": iterations,
-      "deliveryOrderBias": i,
-      "deliveryLikehood": j,
+      "iterations": iterations, // añadido para mas información
+      "deliveryLikehood": i, // era likehoodOfNetworkBias
+      "deliveryOrderBias": j, // era networkOrderBias
       "durations": durations,
+      // eran percentageOfFalsePositives
       "numTrue": numTrue,
       "numFalse": iterations - numTrue,
     });
